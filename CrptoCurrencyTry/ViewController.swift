caf2cbb2509589c1575b7ae43a1eb9f3090013de
Yasin <ysndlklc1989@icloud.com>
2022-05-31 12:18:30 +0300

    last commit

--- CrptoCurrencyTry/ViewController.swift
+++ CrptoCurrencyTry/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     var templistName = ["BTC","ETH"]
     var templistPrice = ["10","20"]
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
